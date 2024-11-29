a6652e43a88b004ff8ffe5c3e3e0413fc1781e42
mrn-dns <marincasdenis@yahoo.com>
2024-11-29 03:08:07 +0000

    Changed how confirmation email is handled since switched from queue to topic.

--- lambdas/confirmation-mailer.ts
+++ lambdas/confirmation-mailer.ts
@@ -1,4 +1,4 @@
-import { SQSHandler } from "aws-lambda";
+import { SNSEvent } from "aws-lambda";
 import { SES_EMAIL_FROM, SES_EMAIL_TO, SES_REGION } from "../env";
 import {
   SESClient,
@@ -18,22 +18,22 @@ type ContactDetails = {
   message: string;
 };
 
-const client = new SESClient({ region: SES_REGION});
+const client = new SESClient({ region: SES_REGION });
 
-export const handler: SQSHandler = async (event: any) => {
+export const handler = async (event: SNSEvent): Promise<void> => {
   console.log("Event ", JSON.stringify(event));
-  for (const record of event.Records) {
-    const recordBody = JSON.parse(record.body);
-    const snsMessage = JSON.parse(recordBody.Message);
 
-    if (snsMessage.Records) {
-      console.log("Record body ", JSON.stringify(snsMessage));
-      for (const messageRecord of snsMessage.Records) {
-        const s3e = messageRecord.s3;
-        const srcBucket = s3e.bucket.name;
-        // Object key may have spaces or unicode non-ASCII characters.
-        const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
-        try {
+  for (const record of event.Records) {
+    try {
+      const snsMessage = JSON.parse(record.Sns.Message);
+
+      if (snsMessage.Records) {
+        console.log("Record body ", JSON.stringify(snsMessage));
+        for (const messageRecord of snsMessage.Records) {
+          const s3e = messageRecord.s3;
+          const srcBucket = s3e.bucket.name;
+          // Object key may have spaces or unicode non-ASCII characters.
+          const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
           const { name, email, message }: ContactDetails = {
             name: "The Photo Album",
             email: SES_EMAIL_FROM,
@@ -41,11 +41,11 @@ export const handler: SQSHandler = async (event: any) => {
           };
           const params = sendEmailParams({ name, email, message });
           await client.send(new SendEmailCommand(params));
-        } catch (error: unknown) {
-          console.log("ERROR is: ", error);
-          // return;
+          console.log(`Email sent for image: s3://${srcBucket}/${srcKey}`);
         }
       }
+    } catch (error: unknown) {
+      console.error("Error processing record:", error);
     }
   }
 };
@@ -61,10 +61,6 @@ function sendEmailParams({ name, email, message }: ContactDetails) {
           Charset: "UTF-8",
           Data: getHtmlContent({ name, email, message }),
         },
-        // Text: {.           // For demo purposes
-        //   Charset: "UTF-8",
-        //   Data: getTextContent({ name, email, message }),
-        // },
       },
       Subject: {
         Charset: "UTF-8",
@@ -88,16 +84,5 @@ function getHtmlContent({ name, email, message }: ContactDetails) {
         <p style="font-size:18px">${message}</p>
       </body>
     </html> 
-  `;
-}
-
- // For demo purposes - not used here.
-function getTextContent({ name, email, message }: ContactDetails) {
-  return `
-    Received an Email. 📬
-    Sent from:
-        👤 ${name}
-        ✉️ ${email}
-    ${message}
   `;
 }
